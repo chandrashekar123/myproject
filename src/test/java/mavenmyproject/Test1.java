e77247556eec673edf197a74e91acb8a4f0d84fa
candra <candra@candra-PC>
2021-01-16 13:56:28 +0530

    first pull.....

---
+++ src/test/java/mavenmyproject/Test1.java
@@ -0,0 +1,39 @@
+package mavenmyproject;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	int a=10;
+    int b=20;
+	
+	@Test
+	
+	public void add() {
+	System.out.println("sum");
+	Assert.assertEquals(20,20);
+		}
+	
+@Test
+	
+	public void sub() {
+	System.out.println("sub");
+	Assert.assertEquals(20,20);
+		}
+	
+@Test
+
+public void div() {
+System.out.println("div");
+Assert.assertEquals(20,20);
+	}
+
+@Test
+
+public void mul() {
+System.out.println("mul");
+Assert.assertEquals(20,20);
+	}
+
+	
+}
